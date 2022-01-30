fa09428e701e4708b93add6940464d15ac6bfc14
martinezbernalpablo@gmail.com <monopatinct@gmail.com>
2022-01-30 12:30:49 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+from types import SimpleNamespace
+
+
+data = SimpleNamespace(**{
+    "bot_token": "Your token here",
+    "database_ip": "X.X.X.X",
+    "database_username": "Your username here",
+    "database_password": "Your password here",
+})
